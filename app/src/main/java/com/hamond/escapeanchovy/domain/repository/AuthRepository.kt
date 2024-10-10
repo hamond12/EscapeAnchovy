6fd5dc87ce456d7367bdd46f355bd450905d2851
hamond12 <chicken010208@gmail.com>
2024-10-10 18:32:55 +0900

    refactor: 로그인 화면 컴포저블 함수 분할

--- app/src/main/java/com/hamond/escapeanchovy/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/hamond/escapeanchovy/domain/repository/AuthRepository.kt
@@ -1,9 +1,7 @@
 package com.hamond.escapeanchovy.domain.repository
 
 import androidx.credentials.GetCredentialResponse
-import com.google.firebase.auth.AuthCredential
 
-// Data Layer
 interface AuthRepository {
     suspend fun googleLogin(result: GetCredentialResponse): Result<String>
 }
