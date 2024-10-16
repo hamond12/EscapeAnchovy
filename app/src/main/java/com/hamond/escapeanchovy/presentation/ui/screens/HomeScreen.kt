9667d4c90f025fd4784d30f7547437149c56a0fd
hamond12 <chicken010208@gmail.com>
2024-10-16 12:01:50 +0900

    feat: 계정의 사용자 이름 출력하기 (임시)

--- app/src/main/java/com/hamond/escapeanchovy/presentation/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/hamond/escapeanchovy/presentation/ui/screens/HomeScreen.kt
@@ -6,19 +6,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
+import com.google.firebase.firestore.FirebaseFirestore
 import com.hamond.escapeanchovy.constants.Routes
 import com.hamond.escapeanchovy.utils.AccountUtils.cancelAutoLogin
+import com.hamond.escapeanchovy.utils.AccountUtils.getUserEmail
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
 
     val context = LocalContext.current
 
-    //val uid = getUid(context)
+    val email = getUserEmail(context)!!
+
+    var userName by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -33,7 +41,31 @@ fun HomeScreen(navController: NavHostController) {
                 popUpTo(Routes.HOME) { inclusive = true }
             }
         }) {
-            Text(text = "Log Out")
+            Text(text = userName)
         }
     }
+
+    getUserNameByEmail(email) { name, error ->
+        if (error == null) {
+            userName = name!!
+        } else {
+            // 에러처리
+        }
+    }
+
 }
+
+fun getUserNameByEmail(email: String, callback: (String?, String?) -> Unit) {
+    val db = FirebaseFirestore.getInstance()
+    db.collection("User")
+        .whereEqualTo("email", email).limit(1).get()
+        .addOnSuccessListener {
+            val document = it.documents.first()
+            val name = document.getString("name")
+            callback(name, null)
+        }
+        .addOnFailureListener {
+            callback(null, it.message)
+        }
+}
+
